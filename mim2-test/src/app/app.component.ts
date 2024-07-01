8d387c4fa332eb10d5123a7b347a0275b604b108
lilybw <lilybwanscher@gmail.com>
2024-07-01 08:40:52 +0200

    JS execution in template

--- mim2-test/src/app/app.component.ts
+++ mim2-test/src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Renderer2, OnInit, Inject, Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template: `
+  <main class="main">
+    <p>Root is below this guy</p>
+    <div id="root"></div>
+  </main>`,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'mim2-test';
+export class AppComponent implements OnInit {
+
+
+
+    constructor(
+        private _renderer2: Renderer2, 
+        @Inject(DOCUMENT) private _document: Document
+    ) { }
+
+    public ngOnInit() {
+
+        let script = this._renderer2.createElement('script');
+        script.type = `text/javascript`;
+        script.text = `
+            {
+                document.getElementById('root').innerHTML = 'Hello World';
+            }
+        `;
+
+        this._renderer2.appendChild(this._document.body, script);
+    }
 }
